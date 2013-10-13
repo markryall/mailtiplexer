f1b408bf37da00359bf01c11edefd5bc71329836
Mark Ryall <mark@ryall.name>
2013-10-13 14:38:26 +1000

    added basic email multiplexing

---
+++ README.md
@@ -0,0 +1,12 @@
+# heroku addons
+
+For receiving mail:
+
+    heroku addons:add cloudmailin:developer
+    heroku addons:open cloudmailin
+    heroku addons:docs cloudmailin
+
+For sending mail:
+
+    heroku addons:add sendgrid:starter
+    heroku addons:docs sendgrid
--- app.rb
+++ app.rb
@@ -1,5 +1,44 @@
 require 'sinatra'
+require 'pony'
+require 'json'
+
+RECIPIENTS = ENV['RECIPIENTS'].split ';'
+EMAIL = ENV['CLOUDMAILIN_FORWARD_ADDRESS']
+USER = EMAIL.split('@').first
+
+Pony.options = {
+  via: :smtp,
+  via_options: {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    domain: 'heroku.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+}
+
+before do
+  if request.request_method == "POST" and request.content_type=="application/json"
+    body_parameters = request.body.read
+    parsed = body_parameters && body_parameters.length >= 2 ? JSON.parse(body_parameters) : nil
+    params.merge!(parsed)
+  end
+end
 
 get '/' do
-  "Hello, world"
+  "There are currently #{RECIPIENTS.count} recipients"
+end
+
+post "/cm#{USER}" do
+  RECIPIENTS.each do |recipient|
+    Pony.mail to: recipient,
+      from: params['envelope']['from'],
+      reply_to: EMAIL,
+      subject: params['headers']['Subject'],
+      body: params['plain'],
+      html_body: params['html']
+  end
+  "Sent #{RECIPIENTS.count} emails"
 end
